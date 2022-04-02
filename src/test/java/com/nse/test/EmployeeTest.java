ffad434362efc969190a13c275c84437061e4894
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-02 17:18:08 +0530

    add employee - please complete

---
+++ src/test/java/com/nse/test/EmployeeTest.java
@@ -0,0 +1,16 @@
+package com.nse.test;
+
+import org.testng.annotations.Test;
+
+import com.nse.base.WebDriverWrapper;
+
+public class EmployeeTest extends WebDriverWrapper {
+	
+	@Test
+	public void addEmployeeTest()
+	{
+		//check assignment docx and complete
+		
+	}
+
+}
