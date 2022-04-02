c6f9c246560a24b02823b1e4c263f24c90e66ed4
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-02 16:41:49 +0530

    invalid credential dataprovider in same class

--- src/test/java/com/nse/test/LoginTest.java
+++ src/test/java/com/nse/test/LoginTest.java
@@ -2,19 +2,30 @@ package com.nse.test;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.nse.base.WebDriverWrapper;
 
 public class LoginTest extends WebDriverWrapper {
-	
-	//create dataprovider
-	//john, john123, Invalid credentials
-	//peter,peter123, Invalid credentials
 
+	@DataProvider
+	public String[][] invalidCredentialData() {
+		String[][] main = new String[2][3];
 
-	@Test
-	public void invalidCredentialTest(String username,String password,String expectedTitle) {
+		main[0][0] = "john";
+		main[0][1] = "john123";
+		main[0][2] = "Invalid credentials";
+
+		main[1][0] = "peter";
+		main[1][1] = "peter123";
+		main[1][2] = "Invalid credentials";
+
+		return main;
+	}
+
+	@Test(dataProvider = "invalidCredentialData")
+	public void invalidCredentialTest(String username, String password, String expectedTitle) {
 		driver.findElement(By.id("txtUsername")).sendKeys(username);
 		driver.findElement(By.id("txtPassword")).sendKeys(password);
 		driver.findElement(By.id("btnLogin")).click();
@@ -22,14 +33,13 @@ public class LoginTest extends WebDriverWrapper {
 		String actualError = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(actualError, expectedTitle);
 	}
-	
+
 	@Test
-	public void validCredentialTest()
-	{
+	public void validCredentialTest() {
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
 		driver.findElement(By.id("btnLogin")).click();
-		//need to provide explicit wait
+		// need to provide explicit wait
 		Assert.assertEquals(driver.getCurrentUrl(), "https://opensource-demo.orangehrmlive.com/index.php/dashboard");
 	}
 
