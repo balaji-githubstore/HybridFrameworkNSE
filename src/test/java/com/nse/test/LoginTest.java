6061b85ef6705269d8bdf204ceceda2177545f4b
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-02 15:54:33 +0530

    validcredential and invalidcredential test

--- src/test/java/com/nse/test/LoginTest.java
+++ src/test/java/com/nse/test/LoginTest.java
@@ -5,21 +5,27 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.nse.base.WebDriverWrapper;
-	
+
 public class LoginTest extends WebDriverWrapper {
-	
+
 	@Test
-	public void invalidCredentialTest()
-	{
+	public void invalidCredentialTest() {
 		driver.findElement(By.id("txtUsername")).sendKeys("john");
 		driver.findElement(By.id("txtPassword")).sendKeys("john123");
 		driver.findElement(By.id("btnLogin")).click();
-		
-		String actualError= driver.findElement(By.id("spanMessage")).getText();
-		
+
+		String actualError = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(actualError, "Invalid credentials");
 	}
 	
-	
+	@Test
+	public void validCredentialTest()
+	{
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		//need to provide explicit wait
+		Assert.assertEquals(driver.getCurrentUrl(), "https://opensource-demo.orangehrmlive.com/index.php/dashboard");
+	}
 
 }
