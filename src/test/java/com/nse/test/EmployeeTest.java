a6940c6b477ccc984edf4be4fe82c37771c305e1
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-04 19:32:54 +0530

    complete remaining add employee test

--- src/test/java/com/nse/test/EmployeeTest.java
+++ src/test/java/com/nse/test/EmployeeTest.java
@@ -1,5 +1,6 @@
 package com.nse.test;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import com.nse.base.WebDriverWrapper;
@@ -9,7 +10,13 @@ public class EmployeeTest extends WebDriverWrapper {
 	@Test
 	public void addEmployeeTest()
 	{
-		//check assignment docx and complete
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		driver.findElement(By.partialLinkText("PIM")).click();
+		driver.findElement(By.linkText("Add Employee")).click();
+		driver.findElement(By.id("firstName")).sendKeys("john");
+		//complete remaining steps
 		
 	}
 
