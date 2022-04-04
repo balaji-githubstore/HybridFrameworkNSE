84167cfd875b33dc74ece121ef64ac0664ff44af
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-04 20:23:40 +0530

    added excel utils - convert sheet to string[][]

---
+++ src/test/java/com/nse/utilities/ExcelUtils.java
@@ -0,0 +1,32 @@
+package com.nse.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtils {
+
+	public static String[][] getSheetIntoTwoDimArray(String path,String sheetName) throws IOException {
+		
+		FileInputStream file = new FileInputStream(path);
+		XSSFWorkbook book = new XSSFWorkbook(file);
+		XSSFSheet sheet = book.getSheet(sheetName);
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		int cellCount = sheet.getRow(0).getPhysicalNumberOfCells();
+		
+		String[][] main = new String[rowCount - 1][cellCount];
+		
+		DataFormatter format = new DataFormatter();
+		for (int r = 1; r < rowCount; r++) {
+			for (int c = 0; c < cellCount; c++) {
+				main[r - 1][c] = format.formatCellValue(sheet.getRow(r).getCell(c));
+			}
+		}
+		
+		return main;
+	}
+
+}
